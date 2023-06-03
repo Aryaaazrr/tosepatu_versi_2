f28fd29c1eb1893c5ef58eeedcf49c3e64028a74
Aryaaazrr <achmadzakariya7@gmail.com>
2023-06-04 02:31:11 +0700

    UPDATE

--- public/js/script.js
+++ public/js/script.js
@@ -23,31 +23,3 @@ hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("hamburger-active");
     navMenu.classList.toggle("hidden");
 });
-
-// import { Chart, initTE } from "tw-elements";
-
-// initTE({ Chart });
-
-// // Chart
-// const dataBar = {
-//     type: "bar",
-//     data: {
-//         labels: [
-//             "Monday",
-//             "Tuesday",
-//             "Wednesday",
-//             "Thursday",
-//             "Friday",
-//             "Saturday",
-//             "Sunday ",
-//         ],
-//         datasets: [
-//             {
-//                 label: "Traffic",
-//                 data: [2112, 2343, 2545, 3423, 2365, 1985, 987],
-//             },
-//         ],
-//     },
-// };
-
-// new Chart(document.getElementById("bar-chart"), dataBar);
